f8a801a850bf8f46db22baf129d0dd00a5f92b6c
Serhii Boiko <boyko0043@gmail.com>
2020-11-28 23:37:20 +0200

    added basic settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import './index.css';
-import * as App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
