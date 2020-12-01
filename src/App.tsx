f68e567257d61430505223f82f5908ae9c120483
Serhii Boiko <boyko0043@gmail.com>
2020-12-01 20:16:20 +0200

    some changes in content.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Header } from './components/Header/Header';
 import { Content } from './components/Content/Content';
 import './styles/_global.scss';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
